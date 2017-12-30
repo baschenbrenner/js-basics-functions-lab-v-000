f639d413acb2a487212f1d417f2a44999ecae603
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-12-30 23:16:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,12 @@ function distanceFromHqInBlocks(location) {
 function distanceFromHqInFeet(location) {
   return distanceFromHqInBlocks(location) * 264
 }
+
+function distanceTravelledInFeet(pickup, dropoff) {
+  if (pickup > dropoff) {
+    return (pickup - dropoff) * 264 }
+  else {
+    return (dropoff - pickup) * 264
+  }
+
+}
