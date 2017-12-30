1404d00bd5fad0858e5ea2f001f5347e8d78ddb3
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-12-30 23:13:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+
+
+distanceFromHqInBlocks(location) {
+  if (location > 42) {
+    return location -42
+  }
+  else {
+    42 - location
+  }
+}
+
+distanceFromHqInFeet() {
+  
+}
