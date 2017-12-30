6ca2b6bc05e3fcb950b74a027ff9c37d0c195361
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-12-30 23:20:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,3 +22,19 @@ function distanceTravelledInFeet(pickup, dropoff) {
   }
 
 }
+
+function calculatesFarePrice(pickup, dropoff) {
+  distance = distanceTravelledInFeet(pickup, dropoff)
+  if (distance <=400) {
+    return 0
+  }
+  else if (distance>400 && distance <= 2000) {
+    return distance * 0.02
+  }
+  else if (distance > 2000 && distance <= 2500) {
+    return 25
+  }
+  else {
+    return 'cannot travel that far'
+  }
+}
